99604e771cf191d2a48bc4379420699067b9f7b6
j.xu <j.xu@Ctrip.com>
2017-10-18 11:28:06 +0800

    web-learning-master

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+//Static server
+gulp.task('browser-sync',function(){
+    browserSync.init({
+        server:{
+            baseDir: "./"
+        }
+    });
+    gulp.watch(["src/**/*.html","src/**/*.js","css/*.css"]).on("change",reload);
+});
+gulp.task('serve',['browser-sync']);
+gulp.task('default',['serve']);
